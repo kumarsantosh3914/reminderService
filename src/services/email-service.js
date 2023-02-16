0e61de19361caba3a8c047fa131f7652723c2724
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-02-17 00:45:19 +0530

    Setup of smtp using nodemailer done

---
+++ src/services/email-service.js
@@ -0,0 +1,19 @@
+const sender = require('../config/emailConfig');
+
+const sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) => {
+    try {
+        const response = await sender.sendMail({
+            from: mailFrom,
+            to: mailTo,
+            subject: mailSubject,
+            text: mailBody
+        });
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {
+    sendBasicEmail,
+}
