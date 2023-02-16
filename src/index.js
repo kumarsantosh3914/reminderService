352d7b7aa8c4e208df01eb6d51db7e8f8200e226
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-02-17 01:26:36 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
-const { sendBasicEmail } = require('./services/email-service');
+// const { sendBasicEmail } = require('./services/email-service');
+
+const cron = require('node-cron');
 
 const setupAndStartServer = () => {
     const app = express();
@@ -13,12 +15,16 @@ const setupAndStartServer = () => {
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
 
-        sendBasicEmail(
-            'support@admin.com',
-            'santoshchhinchholikar@gmail.com',
-            'This is a testing email',
-            'Hey, how are you, I hope you like the suppot'
-        );
+        // sendBasicEmail(
+        //     // 'support@admin.com',
+        //     // 'santoshchhinchholikar@gmail.com',
+        //     // 'This is a testing email',
+        //     // 'Hey, how are you, I hope you like the suppot'
+        // );
+
+        cron.schedule('*/2 * * * *', () => {
+            console.log('running a task evry two minutes');
+        })
     });
 }
 
